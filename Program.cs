79e0735a099ac1c318cbc86c237ab9a71951db58
Michael Mathu <mathumichael17@gmail.com>
2022-04-05 07:15:43 +0300

    prior exercise commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace IfStatement
+{
+    class Ifstatement
+    {
+        static void Main(string[] args)
+        {
+            int score = 100;
+
+            int pointsNeededToPass = 100;
+
+            //The Initial way
+            //bool levelComplete;
+
+            //if(score >= pointsNeededToPass)
+            //levelComplete = true;
+            //else
+            // levelComplete = false;
+
+            //Improved way
+            // bool levelComplete = (score >= pointsNeededToPass);
+
+            //if (levelComplete)
+            // Console.WriteLine("You've beten the level!");
+            //else
+            //Console.WriteLine("Try next time");
+
+            //Exercise
+            int number;
+
+            Console.WriteLine("Enter a  number");
+            string input = Console.ReadLine();
+            number = Convert.ToInt32(input);
+
+            
+
+            
+        }
+    }
+}
